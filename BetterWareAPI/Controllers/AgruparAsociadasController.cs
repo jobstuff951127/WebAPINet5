64ce86e27c938b7d424eddd44dfa66a81173d1c1
jobstuff951127@gmail.com <jobstuff951127@gmail.com>
2020-11-26 13:32:39 -0600

    7.1 Done

---
+++ BetterWareAPI/Controllers/AgruparAsociadasController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BetterWareAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AgruparAsociadasController : ControllerBase
+    {
+      
+        [HttpPost]
+        public Dictionary<string,string[]> Get(Dictionary<string,string> Items)
+        {
+            var dic = new  Dictionary<string,string[]>();
+
+            foreach(var kvp in Items)
+            {
+                if (dic.ContainsKey(kvp.Value))
+                {
+                    var lst = dic[kvp.Value].ToList();
+                    lst.Insert(lst.Count(),kvp.Key);
+                    var myArray = lst.ToArray<string>();
+                    dic[kvp.Value] = myArray;
+                } 
+                else
+                {
+                    dic.Add(kvp.Value,new string[] {kvp.Key});
+                }  
+               
+            }
+            return dic;
+        }
+    }
+}
---
+++ BetterWareAPI/Dist.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BetterWareAPI
+{
+    public class Dist
+    {
+        public string Dst  { get; set; }
+
+        public string[] Asoc { get; set; }
+    }
+}
